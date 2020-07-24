719cf96a42d576e197e03d8b3455665b2b18d5d3
Dev-91 <khk2852@gmail.com>
2020-07-24 10:55:02 +0900

    Ready for deploy3

--- quiz/views.py
+++ quiz/views.py
@@ -1,8 +1,10 @@
+"""view.py"""
 #from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Quiz
 from .serializers import QuizSerializser
+import random
 
 # Create your views here.
 @api_view(['GET'])
@@ -12,6 +14,6 @@ def helloAPI(request):
 @api_view(['GET'])
 def randomQuiz(request, id):
     totalQuizs = Quiz.objects.all()
-    randomQuiz = random.sample(list(totalQuizs), id)
-    serializer = QuizSerializser(randomQuiz, many=True)
+    randomQuizs = random.sample(list(totalQuizs), id)
+    serializer = QuizSerializser(randomQuizs, many=True)
     return Response(serializer.data)
